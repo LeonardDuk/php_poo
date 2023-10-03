a24b35f2ff8c58eb8cf3ea433f6366447fea8508
LeonardDuk <leo554-bp@hotmail.com>
2023-10-02 21:58:14 -0300

    visibilidade de Classes

--- Aula03/index.php
+++ Aula03/index.php
@@ -5,16 +5,19 @@
         <title>Aula 02 -POO</title>
     </head>
     <body>
+        <pre>
         <?php
         require_once 'Caneta.php';
         $c1 = new Caneta;
+        $c1->modelo = "BIC cristal";
         $c1->cor = "Azul";
-        $c1->ponta = 0.5;
+        //$c1->ponta = 0.5 ;
+        // $c1->carga = 99;
+        // $c1->tampada = true;
+        $c1->rabiscar();
         $c1->tampar();
-        
-        //$c1->rabiscar();
-        
         print_r($c1);
+
         echo "<br>";
          $c2 = new Caneta;
         $c2->cor = "verde";
@@ -22,5 +25,6 @@
         $c2->tampar();
         print_r($c2);
         ?>
+        </pre>
     </body>
 </html>
